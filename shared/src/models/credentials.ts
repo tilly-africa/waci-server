582d27af968648fc6ae19756efd3d0f194eeb9ff
JustaFramework <wachirawamunyi@gmail.com>
2022-03-06 00:34:34 +0300

    credentials model update

--- shared/src/models/credentials.ts
+++ shared/src/models/credentials.ts
@@ -2,19 +2,25 @@ import S from 'fluent-json-schema'
 
 export type CredentialView = {
   id: string;
-  type: string | string[];
-  credentialSubject: Object;
+  credentialobject: Object;
   holder: Object;
-  context: Array<Object>;
   issuanceDate: String;
   issuer: string;
-  proof: Object;
-  updatedAt: string
-  createdAt: string
-}
+  updatedAt: string;
+  createdAt: string;
+};
 
 export const credentialView = S.object()
-  .prop('id', S.string().format('uuid'))
-  .prop('updatedAt', S.string().format('date-time'))
-  .prop('createdAt', S.string().format('date-time'))
-  .required(['id', 'type', 'issuer', 'proof', 'credentialSubject', 'context', 'issuanceDate', 'updatedAt', 'createdAt'])
+         .prop('id', S.string().format('uuid'))
+         .prop('updatedAt', S.string().format('date-time'))
+         .prop('createdAt', S.string().format('date-time'))
+         .required([
+           'id',
+           'issuer',
+           'credentialobject',
+           'issuanceDate',
+           'updatedAt',
+           'createdAt',
+         ]);
+
+
